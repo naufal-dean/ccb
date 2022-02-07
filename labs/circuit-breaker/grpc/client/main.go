46918420bafe5af546ea1f9fbb64664fd769ac56
naufal-dean <13518123@std.stei.itb.ac.id>
2022-02-07 17:05:41 +0700

    chore: add client and server

--- labs/circuit-breaker/grpc/client/main.go
+++ labs/circuit-breaker/grpc/client/main.go
@@ -1,5 +1,56 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"os"
 
+	"google.golang.org/grpc"
+	// "google.golang.org/grpc/credentials"
+	// "google.golang.org/grpc/examples/data"
+
+	// "github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/ptypes/empty"
+	"github.com/golang/protobuf/ptypes/wrappers"
+
+	pb "github.com/naufal-dean/labs/circuit-breaker/grpc/test"
+)
+
+var (
+	serverAddr = flag.String("addr", "localhost:50051", "The server address in the format of host:port")
+)
+
+func main() {
+	conn, err := grpc.Dial(*serverAddr, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Failed to dial: %v", err)
+	}
+	defer conn.Close()
+
+	c := pb.NewUsersClient(conn)
+
+	userList, err := c.List(context.Background(), new(empty.Empty))
+	if err != nil {
+		log.Fatalf("Failed to request to server: %v", err)
+	}
+
+	data, _ := json.Marshal(userList)
+	log.Printf("%s\n", data)
+
+	  for {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Request: ")
+		text, _ := reader.ReadString('\n')
+
+		echo, err := c.Echo(context.Background(), &wrappers.BytesValue{Value: []byte(text)})
+		if err != nil {
+			log.Fatalf("Failed to request to server: %v", err)
+		}
+
+		fmt.Printf("Response: %s", string(echo.Value))
+	  }
 }
--- labs/circuit-breaker/grpc/server/main.go
+++ labs/circuit-breaker/grpc/server/main.go
@@ -1,5 +1,88 @@
 package main
 
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	// "google.golang.org/grpc/credentials"
+	// "google.golang.org/grpc/examples/data"
+
+	// "github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/ptypes/empty"
+	"github.com/golang/protobuf/ptypes/wrappers"
+
+	pb "github.com/naufal-dean/labs/circuit-breaker/grpc/test"
+)
+
+var (
+	port = flag.Int("port", 50051, "The server port")
+)
+
+type usersServer struct {
+	pb.UnimplementedUsersServer
+}
+
+func (s *usersServer) Register(ctx context.Context, user *pb.User) (*empty.Empty, error) {
+	log.Println("Received request: Register")
+	return new(empty.Empty), nil
+}
+
+func (s *usersServer) List(ctx context.Context, void *empty.Empty) (*pb.UserList, error) {
+	log.Println("Received request: List")
+	return &pb.UserList{
+		List: []*pb.User{
+			&pb.User{
+				Name:  "test",
+				Email: "test@test.com",
+				Age:   20,
+			},
+			&pb.User{
+				Name:  "test2",
+				Email: "test2@test.com",
+				Age:   202,
+			},
+		},
+	}, nil
+}
+
+func (s *usersServer) Echo(ctx context.Context, data *wrappers.BytesValue) (*wrappers.BytesValue, error) {
+	log.Println("Received request: Echo")
+	return data, nil
+}
+
 func main() {
-  
+	flag.Parse()
+
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	var opts []grpc.ServerOption
+
+	// if *tls {
+	// 	if *certFile == "" {
+	// 		*certFile = data.Path("x509/server_cert.pem")
+	// 	}
+	// 	if *keyFile == "" {
+	// 		*keyFile = data.Path("x509/server_key.pem")
+	// 	}
+	// 	creds, err := credentials.NewServerTLSFromFile(*certFile, *keyFile)
+	// 	if err != nil {
+	// 		log.Fatalf("Failed to generate credentials %v", err)
+	// 	}
+	// 	opts = []grpc.ServerOption{grpc.Creds(creds)}
+	// }
+
+	s := usersServer{}
+
+	grpcServer := grpc.NewServer(opts...)
+	pb.RegisterUsersServer(grpcServer, &s)
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve gRPC: %v", err)
+	}
 }
