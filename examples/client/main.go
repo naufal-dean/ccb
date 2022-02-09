bf2bdd9bed43efed0c748812ac27bf31ea798b5d
naufal-dean <13518123@std.stei.itb.ac.id>
2022-02-09 18:12:01 +0700

    chore(client): add test for open circuits procedure

--- examples/client/main.go
+++ examples/client/main.go
@@ -2,16 +2,42 @@ package main
 
 import (
 	"flag"
+	pb "github.com/naufal-dean/ccb/examples/client/protobuf"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	"log"
-
-	pb "github.com/naufal-dean/ccb/examples/client/protobuf"
 )
 
 var (
 	serverAddr = flag.String("addr", "localhost:50051", "The server address in the format of host:port")
 )
+
+func testGet(c pb.HttpClient) {
+	response, err := c.Get(context.Background(), &pb.GetInput{
+		Url:    "https://api.sampleapis.com/coffee/hot",
+		Header: nil,
+	})
+	if err != nil {
+		log.Fatalf("Failed to request to server: %v", err)
+	}
+	log.Println(string(response.Body))
+}
+
+func testOpenCircuits(c pb.ListenerClient) {
+	response, err := c.OpenCircuits(context.Background(), &pb.ServiceEndpoints{
+		Service: "test-euy",
+		Endpoints: []string{
+			"/124",
+			"/hehe",
+			"/123",
+			"/random-euy",
+		},
+	})
+	if err != nil {
+		log.Fatalf("Failed to request to server: %v", err)
+	}
+	log.Println(response)
+}
 
 func main() {
 	conn, err := grpc.Dial(*serverAddr, grpc.WithInsecure())
@@ -20,30 +46,9 @@ func main() {
 	}
 	defer conn.Close()
 
-	c := pb.NewHttpClient(conn)
+	hc := pb.NewHttpClient(conn)
+	testGet(hc)
 
-	response, err := c.Get(context.Background(), &pb.GetInput{
-		Url:    "https://api.sampleapis.com/coffee/hot",
-		Header: nil,
-	})
-	if err != nil {
-		log.Fatalf("Failed to request to server: %v", err)
-	}
-
-	//data, _ := json.Marshal(response)
-	//log.Printf("%s\n", data)
-	log.Println(string(response.Body))
-
-	//for {
-	//	reader := bufio.NewReader(os.Stdin)
-	//	fmt.Print("Request: ")
-	//	text, _ := reader.ReadString('\n')
-	//
-	//	echo, err := c.Echo(context.Background(), &wrappers.BytesValue{Value: []byte(text)})
-	//	if err != nil {
-	//		log.Fatalf("Failed to request to server: %v", err)
-	//	}
-	//
-	//	fmt.Printf("Response: %s", string(echo.Value))
-	//}
+	lc := pb.NewListenerClient(conn)
+	testOpenCircuits(lc)
 }
