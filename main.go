6ff067256e9e976d62d4be9130f201dbe54ed996
naufal-dean <13518123@std.stei.itb.ac.id>
2022-02-09 18:06:08 +0700

    chore: add temporary test

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"github.com/naufal-dean/ccb/app"
+	"github.com/naufal-dean/ccb/db/domain/status"
 	"github.com/naufal-dean/ccb/httpserver"
 	"github.com/naufal-dean/ccb/listenerserver"
 	"github.com/naufal-dean/ccb/server"
@@ -18,6 +19,15 @@ func main() {
 	addr := fmt.Sprintf("localhost:%d", *port)
 
 	application := app.New(*dbpath)
+
+	// TEST
+	// TODO: remove
+	application.Repositories.Status.Create(status.Status{
+		Service:  "test-euy",
+		Endpoint: "/123",
+		Status:   "CLOSED",
+	})
+	// TEST END
 
 	server.Run(addr, func(grpcServer *grpc.Server) {
 		// Register HttpServer
