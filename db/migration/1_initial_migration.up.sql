2f0adbdce440ca43e048b42e0c69ee91dc135064
naufal-dean <13518123@std.stei.itb.ac.id>
2022-02-14 15:18:23 +0700

    chore: update colname on migration script

--- db/migration/1_initial_migration.up.sql
+++ db/migration/1_initial_migration.up.sql
@@ -1,20 +1,23 @@
+-- TODO: add rd_method
 CREATE TABLE status (
-    service TEXT NOT NULL,
-    endpoint TEXT NOT NULL,
+    rd_service TEXT NOT NULL,
+    rd_endpoint TEXT NOT NULL,
     status TEXT NOT NULL,
-    PRIMARY KEY (service, endpoint)
+    PRIMARY KEY (rd_service, rd_endpoint)
 );
 
 -- TODO: Check if primary key order affect performance
 CREATE TABLE requiring_service (
-   service TEXT NOT NULL,
+   rg_service TEXT NOT NULL,
    endpoint TEXT NOT NULL,
-   PRIMARY KEY (service, endpoint)
+   PRIMARY KEY (rg_service, endpoint)
 );
 
 -- TODO: Check if primary key order affect performance
+-- TODO: add rd_method
 CREATE TABLE required_service (
   endpoint TEXT NOT NULL,
-  service TEXT NOT NULL,
-  PRIMARY KEY (endpoint, service)
+  rd_service TEXT NOT NULL,
+  rd_endpoint TEXT NOT NULL,
+  PRIMARY KEY (endpoint, rd_service, rd_endpoint)
 );
