2d5e5894192b5e34e1bffb4214ed66a244af3e62
naufal-dean <13518123@std.stei.itb.ac.id>
2022-02-14 15:55:58 +0700

    feat(broadcaster): init broadcaster component

---
+++ service/internal/broadcaster.go
@@ -0,0 +1,49 @@
+package internal
+
+import (
+	pb "github.com/naufal-dean/ccb/examples/client/protobuf"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"log"
+)
+
+const (
+	// TODO: get service name from env var or app struct
+	serviceName = "service-test"
+)
+
+func BroadcastOpenCircuits(serverAddr string, endpoints []string) {
+	conn, err := grpc.Dial(serverAddr, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Failed to dial: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewListenerClient(conn)
+
+	_, err = client.OpenCircuits(context.Background(), &pb.ServiceEndpoints{
+		Service:   serviceName,
+		Endpoints: endpoints,
+	})
+	if err != nil {
+		log.Fatalf("Failed to request to server: %v", err)
+	}
+}
+
+func BroadcastCloseCircuits(serverAddr string, endpoints []string) {
+	conn, err := grpc.Dial(serverAddr, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Failed to dial: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewListenerClient(conn)
+
+	_, err = client.CloseCircuits(context.Background(), &pb.ServiceEndpoints{
+		Service:   serviceName,
+		Endpoints: endpoints,
+	})
+	if err != nil {
+		log.Fatalf("Failed to request to server: %v", err)
+	}
+}
