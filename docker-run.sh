97d5742091a6579c3bd37d39269405261f62891f
naufal-dean <13518123@std.stei.itb.ac.id>
2022-02-20 13:20:37 +0700

    chore: add dockher hub user in docker run script

--- docker-run.sh
+++ docker-run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -euox pipefail
 
-IMAGE_NAME="ccb:multistage"
+DOCKER_HUB_USER="naufaldean"
+IMAGE_NAME="ccb:latest"
 
 if [[ "$#" -ge 1 ]] && [[ -n "$1" ]] && [[ "$1" =~ b.* ]]; then
-  docker build --tag "$IMAGE_NAME" .
+  docker build --tag "$DOCKER_HUB_USER/$IMAGE_NAME" .
 fi
 
-docker run --name ccb-dev -p 50051:50051 -it --rm "$IMAGE_NAME" --dbpath ccb.db
+docker run --name ccb-dev -p 50051:50051 -it --rm "$DOCKER_HUB_USER/$IMAGE_NAME" --dbpath ccb.db
